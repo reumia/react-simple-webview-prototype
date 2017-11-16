345fd734d59a314d8268e2c7481437a668d1f0ca
TTS기술담당 조현석 <skt.1970159@sk.com>
2017-11-16 10:59:52 +0900

    Axios setup

---
+++ src/utils/axios.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+/**
+ * Default Properties
+ * @type {{baseURL: string}}
+ */
+axios.defaults = {
+    baseURL: 'https://jsonplaceholder.typicode.com'
+}
+
+/**
+ * Global Error Handling
+ */
+axios.interceptors.response.use(null, function(err) {
+    if (err.status === 401) console.warn('UNAUTHORIZED')
+
+    return Promise.reject(err)
+});
+
+export default axios
