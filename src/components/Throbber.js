3896126fa0f38030c0d2ca0947404869ea440422
TTS기술담당 조현석 <skt.1970159@sk.com>
2017-11-16 16:40:17 +0900

    Throbber 밸리데이션 추가

--- src/components/Throbber.js
+++ src/components/Throbber.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Transition from 'react-transition-group/Transition'
+import PropTypes from 'prop-types'
 
 import './Throbber.scss'
 
@@ -45,3 +46,7 @@ let mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps)(Throbber)
+
+Throbber.propTypes = {
+    isVisible: PropTypes.bool.isRequired
+}
