09fac837ed8d048e74ea11c09d4773d053e6dd07
TTS기술담당 조현석 <skt.1970159@sk.com>
2017-11-16 14:46:47 +0900

    Throbber 추가

---
+++ src/components/Throbber.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Throbber = ({ visible }) => {
+    console.log(visible)
+
+    return (
+        <div className="throbber">
+            트로버
+        </div>
+    )
+}
+
+export default Throbber
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import NoticeIndex from '@/pages/notice/Index'
 import NoticeSingle from '@/pages/notice/Single'
+import Throbber from '@/components/Throbber'
 
 import './index.scss'
 
@@ -9,9 +10,10 @@ import './index.scss'
  * Link 컴퍼넌트 대신 a링크 씀.
  * 어플리케이션이 변경되는 location을 기반으로 새로운 페이지 인스턴스를 생성하거나 history 관리를 담당.
  */
-const BasicRouter = () => (
+const BasicRouter = ({ dispatch }) => (
     <Router>
         <div className="app">
+            <Throbber />
             <nav className="app-nav">
                 <a className="app-nav-item" href="/notice">/notice</a>
                 <a className="app-nav-item" href="/notice/3">/notice/3</a>
